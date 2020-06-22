2e135619078a6a78d4d3b1a9a1d5e5af16951cb4
harsh-gulati-14 <harshgulati1409@gmail.com>
2020-06-22 14:15:17 +0530

    ID DONE FOR USER

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
 const Task = require('./models/task')
-const users = require('./models/user')
+const Users = require('./models/user')
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+
 app.get('/users',(req,res)=>{
-    users.find({}).then((users)=>{
+
+    Users.find({}).then((users)=>{
+
         res.send(users)
+
     }).catch((err)=>{
+
+        res.status(500)
     })
+})
+app.get('/users/:id',(req,res)=>{
+
+        const _id=req.params.id // inbuild function of mongoose to get the id of paramtere  that is convert to id(hexa string)
+
+        Users.findById(_id).then((user)=>{
+            if(!user)
+            {
+                return res.status(404).send()   
+            }
+            res.send(user)
+        }).catch(()=>{
+            res.status(500).send()    
+        })
 })
 app.post('/users', (req, res) => {
 
-    const user = new User(req.body)
+    const user = new Users(req.body)
 
     user.save().then((user) => {
 
